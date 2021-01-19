d247c3fca70fe0bad91ac66d371757f446d27380
remek <remek.kosiorek@gmail.com>
2021-01-19 07:20:28 +0100

    feat: add test_clicl_desktop_grid_1~7
    Test case for each desktop_grid
    
    Checks if hyperlinks are work properly

--- amazsel/tests/test_main_page.py
+++ amazsel/tests/test_main_page.py
@@ -12,13 +12,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+from locators.locators import MainPageLocators
+
 
 class TestMainPage(unittest.TestCase):
 
 	def setUp(self):
 		chrome_options = Options()
 		chrome_options.add_argument("--headless")
-		chrome_options.add_argument('--no-sandbox')
+		chrome_options.add_argument("--no-sandbox")
 		self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
 	def test_main_page(self):
@@ -37,10 +39,55 @@ class TestMainPage(unittest.TestCase):
 	def test_click_desktop_grid_1(self):
 		main_page = MainPage(self.driver)
 		main_page.open_page()
+		title = main_page.check_title_grid_1()
 		main_page.click_desktop_grid_1()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_2(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_2()
+		main_page.click_desktop_grid_2()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_3(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_3()
+		main_page.click_desktop_grid_3()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_4(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_4()
+		main_page.click_desktop_grid_4()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_5(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_5()
+		main_page.click_desktop_grid_5()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_6(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_6()
+		main_page.click_desktop_grid_6()
+		main_page.matches_title_grid(title)
+		
+	def test_click_desktop_grid_7(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		title = main_page.check_title_grid_7()
+		main_page.click_desktop_grid_7()
+		main_page.matches_title_grid(title)
 
-
-
+	def tearDown(self):
+		self.driver.close()
+		
 
 if __name__ == "__main__":
 	unittest.main()
