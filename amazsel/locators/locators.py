b6e2371ae6a363e32cd67d1b784f77658068a95f
remek <remek.kosiorek@gmail.com>
2021-01-19 07:16:15 +0100

    feat: add new locators in MainPageLocators
    Locators: TITLE_GRID_1~7 - headers for each desktop-grid
    
    Locators: TITLE_... headers, for each category in desktop-grid, when href is pressed

--- amazsel/locators/locators.py
+++ amazsel/locators/locators.py
@@ -2,14 +2,29 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
 	LOGO_URL = 'https://www.amazon.com/ref=nav_logo'
-	LOGO = [By.ID, 'nav-logo-sprites']
-	DESKTOP_GRID_1 = (By.XPATH, '//*[@id="desktop-grid-1"]/div/div[3]/a')
-	DESKTOP_GRID_2 = (By.XPATH, '//*[@id="desktop-grid-2"]/div/div[3]/a')
-	DESKTOP_GRID_3 = (By.XPATH, '//*[@id="desktop-grid-3"]/div/div[3]/a')
-	DESKTOP_GRID_4 = (By.XPATH, '//*[@id="desktop-grid-4"]/div/div[3]/a')
-	DESKTOP_GRID_5 = (By.XPATH, '//*[@id="desktop-grid-5"]/div/div[3]/a')
-	DESKTOP_GRID_6 = (By.XPATH, '//*[@id="desktop-grid-6"]/div/div[3]/a')
-	DESKTOP_GRID_7 = (By.XPATH, '//*[@id="desktop-grid-7"]/div/div[3]/a')
-	DESKTOP_GRID_8 = (By.XPATH, '//*[@id="gw-card-layout"]/div[9]/div[1]/div[3]/a')
+	LOGO = (By.ID, "nav-logo-sprites")
+	DESKTOP_GRID_1 = (By.XPATH, "//*[@id='desktop-grid-1']/div/div[3]/a")
+	DESKTOP_GRID_2 = (By.XPATH, "//*[@id='desktop-grid-2']/div/div[3]/a")
+	DESKTOP_GRID_3 = (By.XPATH, "//*[@id='desktop-grid-3']/div/div[3]/a")
+	DESKTOP_GRID_4 = (By.XPATH, "//*[@id='desktop-grid-4']/div/div[3]/a")
+	DESKTOP_GRID_5 = (By.XPATH, "//*[@id='desktop-grid-5']/div/div[3]/a")
+	DESKTOP_GRID_6 = (By.XPATH, "//*[@id='desktop-grid-6']/div/div[3]/a")
+	DESKTOP_GRID_7 = (By.XPATH, "//*[@id='desktop-grid-7']/div/div[3]/a")
+	TITLE_GRID_1 = (By.XPATH, "//*[@id='desktop-grid-1']/div/div[1]/h2")
+	TITLE_GRID_2 = (By.XPATH, "//*[@id='desktop-grid-2']/div/div[1]/h2")
+	TITLE_GRID_3 = (By.XPATH, "//*[@id='desktop-grid-3']/div/div[1]/h2")
+	TITLE_GRID_4 = (By.XPATH, "//*[@id='desktop-grid-4']/div/div[1]/h2")
+	TITLE_GRID_5 = (By.XPATH, "//*[@id='desktop-grid-5']/div/div[1]/h2")
+	TITLE_GRID_6 = (By.XPATH, "//*[@id='desktop-grid-6']/div/div[1]/h2")
+	TITLE_GRID_7 = (By.XPATH, "//*[@id='desktop-grid-7']/div/div[1]/h2")
+	TITLE_AMAZONBASIC = (By.XPATH, "//*[@id='search']/span/div/span/h1/div/div[1]/div/div/span[3]")
+	TITLE_SHOP_BY_CAT = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_ELECTRONICS = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_COMPUTERS_ACCESSORIES  = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_SHOP_TOP  = (By.CSS_SELECTOR, "img[alt='Shop top categories']")
+	TITLE_BEUATY_PICKS = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_GET_FIT = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_DEALS_PROM = (By.XPATH, "//*[@id='a-page']/div[2]/div[2]/div[1]/div[1]/div/h1")
+	TITLE_EASY_RETURNS = (By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[3]/h1")
 
-
+	
