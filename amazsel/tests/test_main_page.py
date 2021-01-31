ab1b9276b1afed4e89ea1e60058c74f5b0e6df1f
remek <remek.kosiorek@gmail.com>
2021-02-01 00:11:17 +0100

    feat: add test case for lang change page

--- amazsel/tests/test_main_page.py
+++ amazsel/tests/test_main_page.py
@@ -1,18 +1,16 @@
 import sys
-import os
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from selenium.webdriver.common.keys import Keys
+from locators.locators import MainPageLocators
 from pages.main_page import MainPage
-import time
 
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-from locators.locators import MainPageLocators
+
 
 
 class TestMainPage(unittest.TestCase):
@@ -23,7 +21,7 @@ class TestMainPage(unittest.TestCase):
 		chrome_options.add_argument("--no-sandbox")
 		self.driver = webdriver.Chrome(ChromeDriverManager().install())
 		self.driver.maximize_window()
-		
+	
 	def test_main_page(self):
 		main_page = MainPage(self.driver)
 		main_page.open_page()
@@ -136,6 +134,13 @@ class TestMainPage(unittest.TestCase):
 		main_page.click_logo_bottom()
 		assert main_page.top_displayed()
 		assert main_page.is_logo_bottom_url_matches()
+		
+	def test_lang_page(self):
+		main_page = MainPage(self.driver)
+		main_page.open_page()
+		main_page.open_lang_change()
+		assert main_page.is_lang_title_matches()
+		main_page.print_title()
 
 	def tearDown(self):
 		self.driver.close()
