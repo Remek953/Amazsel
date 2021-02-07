568374a3147861e52b076686112a9318d8c3fe72
remek <remek.kosiorek@gmail.com>
2021-02-07 19:27:21 +0100

    feat: add more test case for languages and currency change
    Add 2 more test case for language change: german and portuguese
    
    add test case for currency change like: euro, peso, poung
    
    Add new options for chrome

--- amazsel/tests/test_main_page.py
+++ amazsel/tests/test_main_page.py
@@ -15,13 +15,15 @@ class TestMainPage(unittest.TestCase):
 
 	def setUp(self):
 		options = Options()
-		options.add_argument('--profile-directory=Default')
-		options.add_argument('--user-data-dir=C:/Temp/ChromeProfile')
+		#options.add_argument("--profile-directory=Default")
+		#options.add_argument("--user-data-dir=selenium")
 		options.add_argument('start-maximized')  
-		#options.add_argument("--headless") # Runs Chrome in headless mode.
+		options.add_argument("--disable-extensions")
+		options.add_argument("--headless") # Runs Chrome in headless mode.
 		#options.add_argument("--no-sandbox") # Bypass OS security model
 		#options.add_argument('disable-infobars')
-		#options.add_argument("--disable-extensions")
+		#options.add_argument('--disable-gpu')
+		#options.add_argument('--incognito')
 		self.driver = webdriver.Chrome(ChromeDriverManager().install(), options = options)
 
 	def test_main_page(self):
