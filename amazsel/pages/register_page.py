46b8c409e4b427854b80d6372ad4e6e39664aeda
remek <remek.kosiorek@gmail.com>
2021-02-14 14:44:42 +0100

    feat: add functions for check alerts and send text to inputs”

--- amazsel/pages/register_page.py
+++ amazsel/pages/register_page.py
@@ -27,5 +27,39 @@ class Register(BasePage):
 	def is_title_matches(self):
 		return "Amazon Registration" in self.driver.title
 
-		
-		
+	def name_alert(self):
+		text = "Enter your name"
+		return text in self.wait_for_element(RegisterPageLocators.NAME_ALERT).text 
+	def email_alert(self):
+		text = "Enter your email"
+		return text in self.wait_for_element(RegisterPageLocators.EMAIL_ALERT).text
+	def invalid_email_alert(self):
+		text = "Enter a valid email address"
+		return text in self.wait_for_element(RegisterPageLocators.INVALID_EMAIL_ALERT).text  
+	def password_alert(self):
+		text = "Enter your password"
+		return text in self.wait_for_element(RegisterPageLocators.PASSWORD_ALERT).text 
+	def short_password_alert(self):
+		text = "Passwords must be at least 6 characters."
+		return text in self.wait_for_element(RegisterPageLocators.SHORT_PASSWORD_ALERT).text 
+	def re_password_alert(self):
+		text = "Type your password again"
+		return text in self.wait_for_element(RegisterPageLocators.RE_PASSWORD_ALERT).text 
+	def re_password_alert_matches(self):
+		text = "Passwords must match"
+		return text in self.wait_for_element(RegisterPageLocators.MISMATCH_PASSWORD_ALERT).text 
+	def click_acc_button(self):
+		self.wait_to_be_clickable(RegisterPageLocators.CREATE_ACC_BUTTON).click()
+
+	def set_name_input(self, name):
+		self.wait_for_element(RegisterPageLocators.NAME_INPUT).click()
+		self.wait_for_element(RegisterPageLocators.NAME_INPUT).send_keys(name)
+	def set_email_input(self, email):
+		self.wait_for_element(RegisterPageLocators.EMAIL_INPUT).click()
+		self.wait_for_element(RegisterPageLocators.EMAIL_INPUT).send_keys(email)
+	def set_password_input(self, password):
+		self.wait_for_element(RegisterPageLocators.PASSWORD_INPUT).click()
+		self.wait_for_element(RegisterPageLocators.PASSWORD_INPUT).send_keys(password)
+	def set_re_password_input(self, re_password):
+		self.wait_for_element(RegisterPageLocators.RE_PASSWORD_INPUT).click()
+		self.wait_for_element(RegisterPageLocators.RE_PASSWORD_INPUT).send_keys(re_password)
